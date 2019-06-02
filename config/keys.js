5b267c2766aa36fa28201577e6668226c088cea2
vinnikonduru <46607860+vinnikonduru@users.noreply.github.com>
2019-06-02 17:38:44 -0500

    edited keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 
     module.exports ={
-        MongoURI: 'mongodb+srv://vinithaKonduru:<Winnie_33>@cluster0-oubao.mongodb.net/test?retryWrites=true&w=majority'
+        MongoURI: 'mongodb+srv://Your Username:<Your Password>@cluster0-oubao.mongodb.net/test?retryWrites=true&w=majority'
     }
